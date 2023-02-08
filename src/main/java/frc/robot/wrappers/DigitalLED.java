6e6bcff446d8b3dffaa9810598745cab207cc82b
Natalee Zatkoff <nataleezatkoff@gmail.com>
2023-02-08 17:48:46 -0500

    Changed the formatting for the toggle, and cleaned up the comments.

--- src/main/java/frc/robot/wrappers/DigitalLED.java
+++ src/main/java/frc/robot/wrappers/DigitalLED.java
@@ -2,33 +2,37 @@ package frc.robot.wrappers;
 
 import edu.wpi.first.wpilibj.DigitalOutput;
 
+/** 
+ * This class wraps over a {@link DigitalOutput} that represents an LED in the real world.
+ */ 
 public class DigitalLED {
     
     private DigitalOutput LED;
 
-    //constructor 
+    /**
+     * Constructs a DigitalLED object
+     * 
+     * @param port The DIO port that we use
+     */
     public DigitalLED(int port){
 
         LED = new DigitalOutput(port);
     }
 
-    //gets the current value of the LED
+    // Gets the current value of the LED
     public boolean isOn(){ return LED.get();}
     public boolean isOff(){ return !LED.get();}
     public boolean get(){ return LED.get();}
 
-    //sets the value of the LED
+    // Sets the value of the LED
     public void off(){ LED.set(false);}
     public void on(){ LED.set(true);}
 
-    //sets the LED's state to the users will, allows them to change it to whatever
+    /**@param set If set to true, the LED's will be turned on*/
     public void set(boolean set){ LED.set(set);}
 
-    //toggle for the LED
+    /** This function sets the light to the opposite of its current state */
     public void toggle(){
-        if(isOn() == true)
-            off();
-        else 
-            on();
+        set(!get());
     }
 }

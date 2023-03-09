af956771ababdd70d9503d4890b426398279a335
dlozon <lozo6089@kettering.edu>
2023-03-09 11:54:15 -0500

    PP auto no arm ready to test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,23 +3,20 @@ package frc.robot;
 import static frc.robot.Constants.ControllerPorts.DRIVER_PORT;
 import static frc.robot.Constants.DIOPorts.CLAW_LASER_PORT;
 
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.AutonCommandsContainer;
 import frc.robot.commands.DriveCommand;
-import frc.robot.commands.DriveForward;
 import frc.robot.commands.LockDrivetrain;
-import frc.robot.commands.ZeroWheels;
 import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Drivetrain;
 
 /** This is where the drivetrain will be controlled */
 public class RobotContainer {
     private final Drivetrain drivetrain = new Drivetrain();
-    private final Claw claw = new Claw(CLAW_LASER_PORT);
+    private AutonCommandsContainer auton = new AutonCommandsContainer(drivetrain);
+    private Claw claw = new Claw(CLAW_LASER_PORT);
     private XboxController driverController = new XboxController(DRIVER_PORT);
 
     /** Constructs a RobotContainer */
@@ -43,11 +40,7 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
         // Auton for going over the line
-        return new SequentialCommandGroup(
-            new ZeroWheels(drivetrain), 
-            new WaitCommand(5), 
-            new DriveForward(drivetrain, -Units.feetToMeters(16), .3), 
-            new LockDrivetrain(drivetrain));
+        return auton.coneCubeNoBumpAuto();
     }
 
     /** @return The robot's drivetrain */
